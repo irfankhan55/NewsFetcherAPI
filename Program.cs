51420899b306df41d566b66be5e609f4249a4f04
Irfan khan <irfank480@gmail.com>
2025-10-27 04:08:58 +0800

    Initial project setup for Https and Http profiles

---
+++ Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "NewsFetcher API",
+        Description = "A simple .NET 8 Web API for fetching and searching news articles",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Irfan Khan",
+            Email = "irfank480@gmail.com"
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
