393435e44af13c9ef9be415475711c1301c3cc11
Muhammad Irfan Khan <irfank480@gmail.com>
2025-11-15 06:01:29 +0800

    added unit test project

---
+++ NewsFetcherAPITests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using System.Threading.Tasks;
+
+namespace NewsFetcherAPITests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
